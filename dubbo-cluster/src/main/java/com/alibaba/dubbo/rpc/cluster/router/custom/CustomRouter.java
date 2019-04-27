26b0c1351d8d5a1d161152772bd302d521d39180
292292yang <292292yang@183.com>
2019-04-27 23:25:52 +0800

    将获取自定义路由信息改为zk的cache形式

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/custom/CustomRouter.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/custom/CustomRouter.java
@@ -3,6 +3,7 @@ package com.alibaba.dubbo.rpc.cluster.router.custom;
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.logger.Logger;
 import com.alibaba.dubbo.common.logger.LoggerFactory;
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.RpcException;
@@ -11,6 +12,9 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 import java.util.ArrayList;
@@ -27,29 +31,55 @@ public class CustomRouter implements Router {
 
     private static final Logger logger = LoggerFactory.getLogger(CustomRouter.class);
     private static RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+    private static final String PATH = "/dubbo-custom-router/blacklist";
     private static CuratorFramework client;
+    private List<String> limitIpList = new ArrayList<String>();
     private final URL url;
 
     public CustomRouter(URL url) {
         this.url = url;
-        String connectString = url.getBackupAddress();
-        client = CuratorFrameworkFactory.builder()
-                .connectString(connectString)
-                .sessionTimeoutMs(3000)
-                .connectionTimeoutMs(5000)
-                .retryPolicy(retryPolicy)
-                .build();
+        try {
+            String connectString = url.getBackupAddress();
+            client = CuratorFrameworkFactory.builder()
+                    .connectString(connectString)
+                    .sessionTimeoutMs(3000)
+                    .connectionTimeoutMs(5000)
+                    .retryPolicy(retryPolicy)
+                    .build();
+            client.start();
+            final NodeCache cache = new NodeCache(client, PATH);
+            NodeCacheListener listener = new NodeCacheListener() {
+                @Override
+                public void nodeChanged() {
+                    ChildData data = cache.getCurrentData();
+                    if (null != data) {
+                        String limitIpStr = new String(cache.getCurrentData().getData());
+                        //去除limitStr头尾空白
+                        limitIpStr = limitIpStr.trim();
+                        logger.info("###" + PATH + "节点数据:" + limitIpStr);
+                        if (StringUtils.isNotEmpty(limitIpStr)) {
+                            limitIpList = Arrays.asList(limitIpStr.split(","));
+                        }else {
+                            limitIpList = new ArrayList<String>();
+                        }
+                    } else {
+                        limitIpList = new ArrayList<String>();
+                        logger.info("###" + PATH + "节点数据为空###");
+                    }
+                }
+            };
+            cache.getListenable().addListener(listener);
+            cache.start();
+        } catch (Exception e) {
+            logger.error("###自定义路由连接zk失败##失败原因:", e);
+        }
     }
 
     public <T> List<Invoker<T>> route(final List<Invoker<T>> invokers, URL url, final Invocation invocation) throws RpcException {
         List<Invoker<T>> result = new ArrayList<Invoker<T>>();
         for (Invoker invoker : invokers) {
             String providerIp = getIp(invoker.getUrl());
-            // TODO: 2019/4/26 这里需要更改为从zk中获取规则ip
-            long startTimeMills = System.currentTimeMillis();
-            List<String> limitIpList = limitIpList(url);
-            long endTimeMills = System.currentTimeMillis();
-            logger.info("###获取黑名单耗时=" + (endTimeMills - startTimeMills) + "毫秒##limitIpList=" + limitIpList);
+            logger.info("###limitIpList=" + limitIpList);
             if (!limitIpList.contains(providerIp)) {
                 result.add(invoker);
             }
@@ -77,29 +107,5 @@ public class CustomRouter implements Router {
     @Override
     public int compareTo(Router o) {
         return 1;
-    }
-
-    private List<String> limitIpList(URL url) {
-        List<String> limitIpList = new ArrayList<String>();
-        try {
-            if (client.getState() == CuratorFrameworkState.LATENT) {
-                client.start();
-            } else if (client.getState() == CuratorFrameworkState.STOPPED) {
-                String connectString = url.getBackupAddress();
-                CuratorFrameworkFactory.builder()
-                        .connectString(connectString)
-                        .sessionTimeoutMs(3000)
-                        .connectionTimeoutMs(5000)
-                        .retryPolicy(retryPolicy)
-                        .build();
-            }
-            byte[] bytes = client.getData().forPath("/dubbo-custom-router/blacklist");
-            String blackList = new String(bytes);
-            String[] split = blackList.split(",");
-            return Arrays.asList(split);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return limitIpList;
     }
 }
